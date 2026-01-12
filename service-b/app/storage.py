8ba625b76e4d1e4f6ac4223738eff0a5a2d42c0c
Troppmop <troppmop@gmail.com>
2026-01-12 17:19:53 +0200

    storage init

---
+++ service-b/app/storage.py
@@ -0,0 +1,16 @@
+import redis
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+def get_item(item_id:int):
+    cached_item = redis_client.get(f"item_{id}")
+
+    if not cached_item:
+        return {'message':'item not found'}
+    
+    else:
+        return {"item_id": item_id, "cached": True, "data": cached_item.decode('utf-8')}
+
+def set_item(item_id:int, item_data):
+    redis_client.setex(f"item_{item_id}", 3600, item_data)
+    return {'message':'added'}

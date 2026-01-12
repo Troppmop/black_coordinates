20b2e671d865be03a3097420c4f8e6d7d7ab213b
Troppmop <troppmop@gmail.com>
2026-01-12 17:29:20 +0200

    POST /items/

--- service-b/app/routes.py
+++ service-b/app/routes.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 
+from .schemas import Item
+from .storage import get_item, set_item
+
 router = APIRouter()
 
 @router.get('/')
 def root():
     return {'status': 'healthy'}
+
+@router.post('/items/')
+def add_item(item: Item):
+    set_item(item.id, item.data)

d0d02b2e498bc7c61df43184149f9bb9972a2a11
Troppmop <troppmop@gmail.com>
2026-01-13 09:53:02 +0200

    handshake test service-b

--- service-b/app/routes.py
+++ service-b/app/routes.py
@@ -1,14 +1,23 @@
 from fastapi import APIRouter
-
-from .schemas import Item
-from .storage import get_item, set_item
+import requests
+"""from .schemas import Item
+from .storage import get_item, set_item"""
 
 router = APIRouter()
 
 @router.get('/')
 def root():
-    return {'status': 'healthy'}
+    return {'status': 'service-b is healthy'}
 
+@router.get('/service-a')
+def healthcheck_a():
+    response = requests.get(url='localhost:8001/')
+    return response
+
+"""
 @router.post('/items/')
 def add_item(item: Item):
-    set_item(item.id, item.data)
+    set_item(item.ip, item.coords)
+
+@router.get('/items/{ip}')
+def retrieve(ip:str)"""

64edea7ff55b91301c6fc533a9fd2225d5de51d2
kobi <kodigershuni@gmail.com>
2026-01-13 11:32:07 +0200

    added ip validation

--- service-a/app/schemas.py
+++ service-a/app/schemas.py
@@ -1,2 +1,14 @@
 from pydantic.networks import IPvAnyAddress
+from pydantic import BaseModel, ValidationError
+
+
+class Ip(BaseModel):
+    ip_address: IPvAnyAddress
+
+def validate_ip(ip):
+    try:
+        valid_ip = Ip(ip_address=ip)
+        return valid_ip.ip_address
+    except ValidationError as e:
+        return f"\nInvalid input detected! \n {e.errors()}"
 
